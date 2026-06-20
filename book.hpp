5e9a3577b39a0070c06dc7fa9f8d2c52e00de472
Che Diana <chediana263@gmail.com>
2026-06-21 01:21:24 +0800

    Enhance Book class with status and borrowing methods
    Added status management and borrowing functionality.

--- book.hpp
+++ book.hpp
@@ -1,26 +1,30 @@
 #ifndef BOOK_HPP
 #define BOOK_HPP
-#include <string>
+#include <iostream>
+#include <map>
 using namespace std;
 
 class Book{
     string ISBN;
     string title;
     bool isPremium;
+    string status;
 
     public:
        Book(string isbn = " ", string t = " ", bool PS = false);
-       ~Book();
        string getISBN() const;
        string getTitle() const;
        bool getPremiumStatus() const;
        void setISBN(string);
        void setTitle(string);
        void setPremiumStatus(bool);
-       void findBook() const;
-       void availability() const;
+       void borrowBook(map<string, Book> &b);
+       void setAvailability(string s);
+       string setAvailability(string) const;
+       string availability() const;
        void premiumBook() const;
        void displayInfo() const;
+       ~Book();
 };
 
 #endif
